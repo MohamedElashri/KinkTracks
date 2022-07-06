5b382534688ce1871020fbe5c0f650f4ace1c607
Mohamed Elashri <s-mohamed.elashri@zewailcity.edu.eg>
2022-07-06 04:20:23 -0400

    fix pythia runtime issues

--- MC/pythia/kink.cc
+++ MC/pythia/kink.cc
@@ -1,15 +1,8 @@
 #include "Pythia8/Pythia.h"
 #include <iostream>
-#include "TVirtualPad.h"
-#include "TApplication.h"
-#include "TH1.h"         
-#include <TH1F.h>   
-#include "TFile.h"
-#include <TCanvas.h>
-#include <TF1Convolution.h>
 #include <vector>
 #include <iostream>
-
+#include <cmath>
 
 using namespace Pythia8;
 
@@ -22,12 +15,21 @@ int main() {
 
     // Variables Initialization.
     int neta_accepted = 0 ;
+    int ndecay1_accepted = 0 ;
+    int ndecay2_accepted = 0 ;
+    int ndecay3_accepted = 0 ;
+    int ndecay4_accepted = 0 ;
+
 
     // Initialize.
     pythia.init();
-
+    int stau = 1000015;
+    cout << "Lifetime used [mm] =" << scientific << pythia.particleData.tau0(stau) << endl;
     int nEvents = pythia.mode("Main:numberOfEvents");
-    int nAbort = pythia.mode("Main:timesAllowErrors");
+    //int nAbort = pythia.mode("Main:timesAllowErrors");
+    // Histograms
+    Hist angel("kink angle", 10.,-180,180);
+    Hist length("Decay Length", 100.,0,10000);
 
     // Begin event loop.
     for (int iEvent = 0; iEvent < nEvents; ++iEvent) {
@@ -38,32 +40,50 @@ int main() {
         for (int i = 0; i < event.size(); ++i) {
             int idAbs = event[i].idAbs(); // call particles by ID
             double eta = event[i].eta(); //  eta (pseudorapidity)
-            if (event[i].id() == 1000015) { // if the particle is a stau
-                if (event[i].eta() > 2 && event[i].eta() < 5) {  // if the particle is in the LHCb eta range
-                neta_accepted++;
-                // Find the first daughter of the stau.
-                int iDau1 = event[i].daughter1();
-                int iDau2 = event[i].daughter2();
-            cout << "First daughter: " << iDau1 << " Second daughter: " << iDau2 << endl;
-            // calcalate the kink angle.
-            double kink_angle = atan((event[iDau2].y() - event[iDau1].y()) / (event[iDau2].x() - event[iDau1].x()));
-            cout << "Kink angle: " << kink_angle << endl;
-            // calculate the kink length.
-            double kink_length = sqrt(pow(event[iDau2].x() - event[iDau1].x(), 2) + pow(event[iDau2].y() - event[iDau1].y(), 2));
-            cout << "Kink length: " << kink_length << endl;
+            if (idAbs == 1000015) { // if the particle is a stau
+                if (eta > 2 && eta < 5) {  // if the particle is in the LHCb eta range
+                   neta_accepted++;
+                // Find the daughters of the stau.
+                   int iDau1 = event[i].daughter1();
+                   int iDau2 = event[i].daughter2();
+            //cout << "First daughter: " << iDau1 << " Second daughter: " << iDau2 << endl;
+            // calcaulte the kink angle if the first daughter is a tau
+                   if (event[iDau1].idAbs() == 15) {
+                       double kink_angle_rad = event[i].phi() - event[iDau1].phi();
+                       double kink_angle = kink_angle_rad * 180.0 / 3.14159;
+                       cout << "Kink angle: " << kink_angle << endl;
+                          angel.fill(kink_angle);
+                   }
+            // calcualte the kink angle if the second       
+                   if (event[iDau2].idAbs() == 15) {
+                       double kink_angle_rad_2 = event[i].phi() - event[iDau2].phi();
+                       double kink_angle_2 = kink_angle_rad_2 * 180.0 / 3.14159;
+                       cout << "Kink angle: " << kink_angle_2<< endl;
+                          angel.fill(kink_angle_2);
+                   }                   
+
+
+
+
             // calculate the decay length inside LHCb.
-            double dist = event[i].vDec().pAbs();  
+            //double dist = event[i].vDec().pAbs();
+                   double dist = sqrt(pow(event[i].xDec(),2) + pow(event[i].yDec(),2) + pow(event[i].zDec(),2));
+                   length.fill(dist);  
+                   cout << "Decay length: " << dist << endl;
 
-            
-
-
-
-
-
-
-
-
-
+                // determine if stau decays between 1000 mm and 2000 mm.
+                   if (dist > 13000 && dist < 15300) {
+                      ndecay1_accepted++;
+                    }
+                    if (dist > 15000 && dist < 18700) {
+                      ndecay2_accepted++;
+                    }
+                    if (dist > 16300 && dist < 18700) {
+                        ndecay3_accepted++;
+                    }
+                    if (dist > 7800 && dist < 9500) {
+                        ndecay4_accepted++;
+                    }
 
                 }
 
@@ -72,6 +92,25 @@ int main() {
     }
     // Statistics.
     pythia.stat();
+    cout << endl << nEvents << " events generated. " << neta_accepted
+    << " events passed eta cut." << endl;
+    cout << endl << nEvents << " events generated. " << ndecay1_accepted
+    << " events passed between M1-M2 + eta ." << endl;
+    cout << endl << nEvents << " events generated. " << ndecay2_accepted
+    << " events passed between M2-M5 + eta ." << endl;
+    cout << endl << nEvents << " events generated. " << ndecay3_accepted
+    << " events passed between M3-M5 + eta ." << endl;
+    cout << endl << nEvents << " events generated. " << ndecay4_accepted
+    << " events passed between T1-T3 + eta ." << endl;
+
+    HistPlot hpl("DecayAngle");
+    hpl.frame( "DecayAnglePlot", "Stau>tau (100 GeV)", "angle (degrees)","Entries");
+    hpl.add(angel, "h,red", "KinkAngle");
+    hpl.plot();
+    HistPlot hpl2("DecayLength");
+    hpl2.frame( "DecayLengthPlot", "Stau>tau (100 GeV)", "Length (mm)","Entries");
+    hpl2.add(length, "h,blue", "DecayLength");
+    hpl2.plot();
 
     // Done.
     return 0;
